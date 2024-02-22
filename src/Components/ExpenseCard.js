fe6a243511dd770b6a74da46b9e10e607f0cbc27
firas hamila <firas.hm.1996@gmail.com>
2024-02-22 14:57:46 +0100

    vv

--- src/Components/ExpenseCard.js
+++ src/Components/ExpenseCard.js
@@ -1,15 +1,19 @@
 import "./ExpenseCard.css";
 
 const ExpenseCard = ({ title, price, date }) => {
+  const year = date.getFullYear();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className="expense-card">
-      <div>
-        <div>month</div>
-        <div>year</div>
-        <div>day</div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-card__info">
         <label>{title}</label>
+        <button>Update Title !!!!</button>
         <div className="expense-card__price">$ {price}</div>
       </div>
     </div>
