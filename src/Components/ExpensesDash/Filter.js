7b7afc5b7905a7f08de1109ad60a0bb5e252d180
firas hamila <firas.hm.1996@gmail.com>
2024-02-29 14:20:36 +0100

    tr

---
+++ src/Components/ExpensesDash/Filter.js
@@ -0,0 +1,12 @@
+import "./Filter.css";
+
+const Filter = () => {
+  return (
+    <div className="filter">
+      <label>Filter by year</label>
+      <select></select>
+    </div>
+  );
+};
+
+export default Filter;
